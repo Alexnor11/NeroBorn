a1cfc00971eb8543e0d5b0b9488b4a68b1f177e8
Alexnor11 <advance114@gmail.com>
2025-10-27 15:12:53 +0300

    Adding a jump

--- Assets/_Scripts/PlayerBehavior.cs
+++ Assets/_Scripts/PlayerBehavior.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
+
 
     private float vInput;
     private float hInput;
     private Rigidbody _rb;
+    private CapsuleCollider _col;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
 
     private void Update()
@@ -23,8 +30,13 @@ public class PlayerBehavior : MonoBehaviour
 
         //this.transform.Translate(Vector3.forward * vInput *  Time.deltaTime);
         //this.transform.Rotate(Vector3.up * hInput  * Time.deltaTime);
-    }
 
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
+    }    
+    
     private void FixedUpdate()
     {
         Vector3 rotation = Vector3.up * hInput;
@@ -32,5 +44,16 @@ public class PlayerBehavior : MonoBehaviour
 
         _rb.MovePosition(transform.position + transform.forward * vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x,
+            _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround,
+            groundLayer, QueryTriggerInteraction.Ignore);
+        
+        return grounded;
     }
 }
