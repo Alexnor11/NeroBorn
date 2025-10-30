09627e23969300f5ab5fdf98d8f2475f820a87a9
Alexnor11 <advance114@gmail.com>
2025-10-30 16:55:41 +0300

    Enemy Mechanics

--- Assets/_Scripts/EnemyBehaviour.cs
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -1,13 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    public Transform patrolRoute;
+    public List<Transform> locations;
+    public Transform player;
+
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+        
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    private void Update()
+    {
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach(Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if(locations.Count == 0) return;
+        
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.name == "Player")
         {
+            agent.destination = player.position;
             print("Player detected ­ attack!");
         }
     }
