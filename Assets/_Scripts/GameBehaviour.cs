1c55346a88634d2a2e67730a9edd55c9df69bbe4
Alexnor11 <advance114@gmail.com>
2026-02-07 17:04:15 +0300

    Overloading methods

--- Assets/_Scripts/GameBehaviour.cs
+++ Assets/_Scripts/GameBehaviour.cs
@@ -80,7 +80,7 @@ public class GameBehaviour : MonoBehaviour
             if(GUI.Button(new Rect(Screen.width / 2 -100, 
                 Screen.height /2 - 50, 200, 100),"You lose..."))
             {
-                Utilities.RestartLevel();
+                Utilities.RestartLevel(0);
             }
         }
     }
