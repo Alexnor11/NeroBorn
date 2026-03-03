1512699bd2eb4db8b916b88ba34aed7a08eadeb4
Alex Nor <advance114@gmail.com>
2026-03-03 18:29:24 +0300

    character details

--- Assets/_Scripts/C#OOP/Character.cs
+++ Assets/_Scripts/C#OOP/Character.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class Character
 {
-
+    public string name;
+    public int exp = 0;
 }
--- Assets/_Scripts/C#OOP/LearningCurve.cs
+++ Assets/_Scripts/C#OOP/LearningCurve.cs
@@ -7,6 +7,6 @@ public class LearningCurve : MonoBehaviour
     void Start()
     {
         Character hero = new Character();
+        Debug.LogFormat("Hero: {0} - {1}", hero.name, hero.exp);
     }
-
 }
