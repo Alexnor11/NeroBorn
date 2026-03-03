7bfa9d5f9278ee5059c653ebb41be08f58c08de9
Alex Nor <advance114@gmail.com>
2026-03-03 18:20:58 +0300

    Creating a new character

---
+++ Assets/_Scripts/C#OOP/LearningCurve.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{    
+    void Start()
+    {
+        Character hero = new Character();
+    }
+
+}
